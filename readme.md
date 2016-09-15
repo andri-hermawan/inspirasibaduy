1fe3b189e42507d4249f4e6157be652f40ebb06d
swd <andri.hermawan.skom@gmail.com>
2016-09-15 16:13:01 +0700

    Version 1

---
+++ readme.md
@@ -0,0 +1,21 @@
+This is a sample website using express js and mysql
+
+## Installation
+*for newbies : Clone or download zip to your machine then hit this :
+
+	npm install
+
+## Configuration (database)
+app.js
+
+        host: 'localhost',
+        user: 'root',
+        password : 'you password',
+        port : 3306, //port mysql
+        database:'you database'	
+
+## START
+node app.js
+
+Happy Coding 
+Andri Hermawan
---
+++ routes/index.js
@@ -0,0 +1,52 @@
+
+/*
+ * GET home page.
+ */
+
+exports.home = function(req, res){
+
+  req.getConnection(function(err,connection){
+       
+        var query = connection.query('SELECT * FROM t_artikel',function(err,rows)
+        {
+            if(err)
+                console.log("Error Selecting : %s ",err );
+     
+            res.render('home',{
+            	
+            	data:rows,
+            	title:"Terimakasih Sudah Berkunjung"});
+         	});
+         
+         //console.log(query.sql);
+    });
+  
+};
+
+
+
+exports.single_page = function(req, res){
+    
+    var episode_number = req.params.episode_number;
+    
+    req.getConnection(function(err,connection){
+       
+        var query = connection.query('SELECT * FROM t_artikel WHERE episode_number = ?',[episode_number],function(err,rows)
+        {
+            
+            if(err)
+                console.log("Error Selecting : %s ",err );
+     
+            res.render('single_page',{
+            	title:"Edit Customers - Node.js",
+            	data:rows});
+         });
+         
+         //console.log(query.sql);
+    }); 
+};
+
+exports.notFound =  function(req, res){
+    res.send('Anda tidak di izinkan untuk melihat halaman ini');
+};
+
